46a20d3ce108c7e271630a5c7207240754487465
Sulenny <sulennyu@usf.edu>
2025-09-12 10:02:11 -0400

    first commit

---
+++ cc_3a.js
@@ -0,0 +1,8 @@
+// Coding Challenge 3a
+
+customers = [
+    {name:"Jane Doe",email:"janedoe@gmail.com",purchases:["purse","shoes","Lipstick"]},
+    {name:"John Smith", email:"johnsmith@gmail.com",purchases:["shorts","volleyball","sunglasses"]},
+    {name:"Bella James", email:"bellajames@gmail.com",purchases:["skirt","socks","bracelet"]}
+]
+
