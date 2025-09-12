16bc36982964ebea21c011b2e5638e109d5c94b9
Sulenny <sulennyu@usf.edu>
2025-09-12 11:29:28 -0400

    testing out console

--- cc_3a.js
+++ cc_3a.js
@@ -1,8 +1,10 @@
 // Coding Challenge 3a
 
-customers = [
+ customers = [
     {name:"Jane Doe",email:"janedoe@gmail.com",purchases:["purse","shoes","Lipstick"]},
     {name:"John Smith", email:"johnsmith@gmail.com",purchases:["shorts","volleyball","sunglasses"]},
     {name:"Bella James", email:"bellajames@gmail.com",purchases:["skirt","socks","bracelet"]}
 ]
+
+customers.forEach(obj =>console.log(`Name: ${obj.name} | Email: ${obj.email} | Purchases: ${obj.purchases}`) );
 
