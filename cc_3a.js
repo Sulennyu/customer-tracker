3cbac1d3c070ebe3cd70097f9924c9db926530c0
Sulenny <sulennyu@usf.edu>
2025-09-12 17:12:29 -0400

    new customer added

--- cc_3a.js
+++ cc_3a.js
@@ -1,10 +1,23 @@
 // Coding Challenge 3a
 
- customers = [
+const customers = [
     {name:"Jane Doe",email:"janedoe@gmail.com",purchases:["purse","shoes","Lipstick"]},
     {name:"John Smith", email:"johnsmith@gmail.com",purchases:["shorts","volleyball","sunglasses"]},
     {name:"Bella James", email:"bellajames@gmail.com",purchases:["skirt","socks","bracelet"]}
-]
-
+];
+console.log("=== Customers: Initial ===");
 customers.forEach(obj =>console.log(`Name: ${obj.name} | Email: ${obj.email} | Purchases: ${obj.purchases}`) );
+
+const newObject = {name:"Arthur Young",email:"arthuryoung@gmail.com",purchases:["notebook","pencil","eraser"]};
+customers.push(newObject);
+console.log("\nAdded customer:", `${newObject.name} | ${newObject.email} | ${newObject.purchases}`);
+
+
+
+
+
+
+
+
+
 
